878a8252e110135b97f59e0fa9b9b69b6803b6e1
lby-magicbook <1508201988@qq.com>
2025-11-08 11:46:53 +0800

    feat:项目初始化

---
+++ src/main/java/com/lbytech/lbytechAiAgent/LbytechAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.lbytech.lbytechAiAgent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbytechAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LbytechAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lbytech/lbytechAiAgent/LbytechAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lbytech.lbytechAiAgent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbytechAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
