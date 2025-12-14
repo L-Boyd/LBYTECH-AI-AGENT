79dc89206318ff9ea41c2a79cb1068e5d252b615
lby-magicbook <1508201988@qq.com>
2025-12-14 17:19:34 +0800

    feat:文件保存位置常量（为后续工具调用-保存文件做准备）

---
+++ src/main/java/com/lbytech/lbytechAiAgent/constant/FileConstant.java
@@ -0,0 +1,13 @@
+package com.lbytech.lbytechAiAgent.constant;
+
+/**
+ * 文件常量
+ */
+public interface FileConstant {
+
+    /**
+     * 文件保存目录
+     */
+    String FILE_SAVE_PATH = System.getProperty("user.dir") + "/tmp/file";
+
+}
