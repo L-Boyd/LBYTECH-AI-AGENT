6341dd84171434dadad6078d48e23b5e8901af42
lby-magicbook <1508201988@qq.com>
2025-12-20 17:40:48 +0800

    feat:图像搜索mcp服务初始化

---
+++ lby-image-search-mcp-server/src/main/java/com/lbytech/lbyimagesearchmcpserver/LbyImageSearchMcpServerApplication.java
@@ -0,0 +1,13 @@
+package com.lbytech.lbyimagesearchmcpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbyImageSearchMcpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LbyImageSearchMcpServerApplication.class, args);
+    }
+
+}
---
+++ lby-image-search-mcp-server/src/test/java/com/lbytech/lbyimagesearchmcpserver/LbyImageSearchMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lbytech.lbyimagesearchmcpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbyImageSearchMcpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
