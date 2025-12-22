887ff22c2f5b352b93f9412a4d6eaf4339ff3302
lby-magicbook <1508201988@qq.com>
2025-12-22 11:10:50 +0800

    feat:图片搜索mcp服务端注册

--- lby-image-search-mcp-server/src/main/java/com/lbytech/lbyimagesearchmcpserver/LbyImageSearchMcpServerApplication.java
+++ lby-image-search-mcp-server/src/main/java/com/lbytech/lbyimagesearchmcpserver/LbyImageSearchMcpServerApplication.java
@@ -1,7 +1,11 @@
 package com.lbytech.lbyimagesearchmcpserver;
 
+import com.lbytech.lbyimagesearchmcpserver.tools.ImageSearchTool;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
@@ -10,6 +14,18 @@ public class LbyImageSearchMcpServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LbyImageSearchMcpServerApplication.class, args);
+    }
+
+    /**
+     * 系统启动时，自动注册图片搜索工具
+     * @param imageSearchTool
+     * @return
+     */
+    @Bean
+    public ToolCallbackProvider imageSearchTools(ImageSearchTool imageSearchTool) {
+        return MethodToolCallbackProvider.builder()
+                .toolObjects(imageSearchTool)
+                .build();
     }
 
 }
